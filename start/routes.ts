48be4163dc96fc1a36c8a9efd50ccc32dcdb8038
Howard Lopez <howardplopez@gmail.com>
2025-08-07 10:51:37 -0700

    Add route aliases and new movie views

--- start/routes.ts
+++ start/routes.ts
@@ -10,10 +10,23 @@
 import router from '@adonisjs/core/services/router'
 import { HttpContext } from '@adonisjs/core/http'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
 
 router.get('/movies', async function (ctx: HttpContext) {
-
-    ctx.view.share({ movie: "My awesome move to share" })
     return ctx.view.render('pages/movies')
-})
+}).as('movies.index')
+
+router.get('/movies/my-movie', (ctx: HttpContext) => {
+    ctx.view.share({ movie: "My awesome movie to share" })
+    return ctx.view.render('pages/movies')
+}).as('movies.show')
+
+router.get('/movies/create', () => {}).as('movies.create')
+
+router.post('/movies', () => {}).as('movies.store')
+
+router.get('/movies/my-movie/edit', () => {}).as('movies.edit')
+
+router.put('/movies/my-movie', () => {}).as('movies.update')
+
+router.delete('/movies/my-movie', () => {}).as('movies.destroy')
