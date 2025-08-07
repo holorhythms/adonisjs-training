d17463d5013802d1d20fa6da79d728deba495493
Howard Lopez <howardplopez@gmail.com>
2025-08-07 12:57:59 -0700

    Add route params, create file URLs for movies

--- start/routes.ts
+++ start/routes.ts
@@ -9,16 +9,20 @@
 
 import router from '@adonisjs/core/services/router'
 import { HttpContext } from '@adonisjs/core/http'
+import app from '@adonisjs/core/services/app'
+import * as fs from 'node:fs/promises'
 
 router.on('/').render('pages/home').as('home')
 
 router.get('/movies', async function (ctx: HttpContext) {
-    return ctx.view.render('pages/movies')
+    return ctx.view.render('pages/movies/index')
 }).as('movies.index')
 
-router.get('/movies/my-movie', (ctx: HttpContext) => {
-    ctx.view.share({ movie: "My awesome movie to share" })
-    return ctx.view.render('pages/movies')
+router.get('/movies/:slug', async (ctx: HttpContext) => {
+    const url: URL = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+    const movie = await fs.readFile(url, 'utf8')
+
+    return ctx.view.render('pages/movies/show',  { movie })
 }).as('movies.show')
 
 router.get('/movies/create', () => {}).as('movies.create')
